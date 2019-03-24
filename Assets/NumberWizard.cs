a5780caff0c9ec06a570daa4b85d9dacd5e74cf2
Jenny A <untmdsprt@gmail.com>
2019-03-24 10:40:58 -0500

    Print To Console with Debug.Log()

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to the Number Wizard!");
+        Debug.Log("Please pick a number between ");
+        Debug.Log("Highest number is: ");
+        Debug.Log("Lowest number is: ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
