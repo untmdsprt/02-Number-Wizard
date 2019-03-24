4d53e5b0502456aef7aa5a3c054af1fb51706d61
Jenny A <untmdsprt@gmail.com>
2019-03-24 16:36:46 -0500

    Calculate Guess Variable

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-    int max = 1000;
-    int min = 1;
+    int maxNumber = 1000;
+    int minNumber = 1;
     int guess = 500;
 
     // Start is called before the first frame update
@@ -13,10 +13,11 @@ public class NumberWizard : MonoBehaviour
     {
         Debug.Log("Welcome to the Number Wizard!");
         Debug.Log("Please pick a number, but don't tell me what it is.");
-        Debug.Log("The highest number you can pick is: " + max);
-        Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("The highest number you can pick is: " + maxNumber);
+        Debug.Log("The lowest number you can pick is: " + minNumber);
         Debug.Log("Tell me if your number is higher or lower than " + guess);
         Debug.Log("Push UpArrow = Higher, Push DownArrow = Lower, Push Enter = Correct");
+        maxNumber += 1;
     }
 
     // Update is called once per frame
@@ -25,23 +26,23 @@ public class NumberWizard : MonoBehaviour
         //Detect when the up arrow key is pressed down
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {   
-            Debug.Log("UpArrow key was pressed.");
-            min = guess;
-            Debug.Log(guess);
+            minNumber = guess;
+            guess = (maxNumber + minNumber) / 2;
+            Debug.Log("Is it higher or lower than..." + guess);
         }
         
         //Detect when the down arrow key is pressed down
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {    
-            Debug.Log("DownArrow key was pressed.");
-            max = guess;
-            Debug.Log(guess);
+            maxNumber = guess;
+            guess = (maxNumber + minNumber) / 2;
+            Debug.Log("Is it higher or lower than..." + guess);
         }
         
         //Detect when the Return key is pressed down
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Return key was pressed.");
+            Debug.Log("Correct!!");
         }
     }
 }
