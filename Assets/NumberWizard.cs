2d3984da2b7dbd77b56fba9439ff06fa86373505
Jenny A <untmdsprt@gmail.com>
2019-03-24 16:15:35 -0500

    Scope and Context of Variables

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,40 +4,44 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+
     // Start is called before the first frame update
     void Start()
     {
-        int maxnumber = 1000;
-        int minnumber = 1;
-
-
         Debug.Log("Welcome to the Number Wizard!");
         Debug.Log("Please pick a number, but don't tell me what it is.");
-        Debug.Log("The highest number you can pick is: " + maxnumber);
-        Debug.Log("The lowest number you can pick is: " + minnumber);
-        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than " + guess);
         Debug.Log("Push UpArrow = Higher, Push DownArrow = Lower, Push Enter = Correct");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Detect when the Return key is pressed down
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            Debug.Log("Return key was pressed.");
-        }
-
         //Detect when the up arrow key is pressed down
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {   
             Debug.Log("UpArrow key was pressed.");
+            min = guess;
+            Debug.Log(guess);
         }
-
+        
         //Detect when the down arrow key is pressed down
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {    
             Debug.Log("DownArrow key was pressed.");
+            max = guess;
+            Debug.Log(guess);
+        }
+        
+        //Detect when the Return key is pressed down
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Return key was pressed.");
         }
     }
 }
