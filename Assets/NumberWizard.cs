20fcaeeafd59e4b1399ec2d17ee58da7706e86f9
Jenny A <untmdsprt@gmail.com>
2019-03-24 13:59:12 -0500

    Respond To Player Input

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -15,11 +15,29 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("Please pick a number, but don't tell me what it is.");
         Debug.Log("The highest number you can pick is: " + maxnumber);
         Debug.Log("The lowest number you can pick is: " + minnumber);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push UpArrow = Higher, Push DownArrow = Lower, Push Enter = Correct");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Detect when the Return key is pressed down
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Return key was pressed.");
+        }
+
+        //Detect when the up arrow key is pressed down
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {   
+            Debug.Log("UpArrow key was pressed.");
+        }
+
+        //Detect when the down arrow key is pressed down
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {    
+            Debug.Log("DownArrow key was pressed.");
+        }
     }
 }
